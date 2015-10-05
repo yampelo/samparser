5f766bfbd06c09e86310cfe66e79f8b1d7642577
Omer Yampel <omeryampel@gmail.com>
2015-10-05 16:04:07 +0300

    group support
    Added support for groups & group memeberships

--- samparser.py
+++ samparser.py
@@ -2,6 +2,30 @@ import struct, datetime, argparse
 from collections import OrderedDict
 from Registry import Registry #python-registry
 
+#Used for the SAMPARSE function to decode binary data to SID's
+def binary_to_sid(binary_data):
+	if len(binary_data) < 12:
+		return ''
+	elif len(binary_data) == 12:
+		rev = struct.unpack("<B", binary_data[0:1])[0]
+		#dash = struct.unpack("<B", binary_data[1:1])[0]
+		authid = str(binary_data[2:8].encode("hex")).replace("00000000000", '')
+		sub = struct.unpack("<L", binary_data[8:12])[0]
+		return "S-"+str(rev)+"-"+str(authid)+"-"+str(sub)
+	elif len(binary_data) > 12:
+		rev = struct.unpack("<B", binary_data[0:1])[0]
+		authid = str(binary_data[2:8].encode("hex")).replace("00000000000", '')
+		sub = struct.unpack("<LLLL", binary_data[8:24])
+		sub = map(str,sub)
+		rid = struct.unpack("<L", binary_data[24:30])[0]
+		return "S-"+str(rev)+"-"+str(authid)+"-"+'-'.join(sub)+"-"+str(rid)
+
+def sid_to_username(sid, software):
+	try:
+		key = software.open("Microsoft\\Windows NT\\CurrentVersion\\ProfileList\\"+sid)
+	except:
+		return None
+	return key.value("ProfileImagePath").value().split("\\")[-1]
 
 #Converts windows FILETIME (2 DWORDS) to datetime object formatted by strftime
 def getTime(low, high):
@@ -20,7 +44,8 @@ def getTime(low, high):
 #parses the hive and returns it as a dictionary.
 def samparse(samhive):
 
-	results = {}
+	results = OrderedDict()
+	results['users'] = OrderedDict()
 
 	#Ok this one is a bit more diffuclt. the data in here is in Binary data which means we need to unpack all of it.
 	#The SAM file is structed like this
@@ -63,7 +88,7 @@ def samparse(samhive):
 		#We Use this to get the account name and timestamp
 		if x.name() == "Names":
 			for a in x.subkeys():
-				results[a.name()]['Account Created Date'] = a.timestamp().strftime('%d %B %Y - %H:%M:%S')
+				results['users'][a.name()]['Account Created Date'] = a.timestamp().strftime('%d %B %Y - %H:%M:%S')
 		else:
 			for a in x.values():
 				#F comes before V, and since we're using usernames, F will store in a temporary dict
@@ -110,42 +135,100 @@ def samparse(samhive):
 					profilepath_lngth = struct.unpack("<L", data[112:116])[0]
 					workstations_ofst = struct.unpack("<L", data[120:124])[0]
 					workstations_lngth = struct.unpack("<L", data[124:128])[0]
-					# lmpwhash_ofset = struct.unpack("<L", data[156:160])[0]		
+					# lmpwhash_ofset = struct.unpack("<L", data[156:160])[0]	Can enable this, contains the NT and LM hashes	
 					# lmpwhash_lngth = struct.unpack("<L", data[160:164])[0]
 					# ntpwhash_ofset = struct.unpack("<L", data[168:172])[0]		
 					# ntpwhash_lngth = struct.unpack("<L", data[172:176])[0]
 
 					username = data[(username_ofst+0xCC):(username_ofst+0xCC + username_lngth)].replace('\x00','')
 
-					results[username] = OrderedDict()
+					results['users'][username] = OrderedDict()
 
-					results[username]['Full Name'] = data[(fullname_ofst+0xCC):(fullname_ofst+0xCC + fullname_lngth)]
-					results[username]['Comment'] = data[(comment_ofst+0xCC):(comment_ofst+0xCC + comment_lngth)]
+					results['users'][username]['Full Name'] = data[(fullname_ofst+0xCC):(fullname_ofst+0xCC + fullname_lngth)]
+					results['users'][username]['Comment'] = data[(comment_ofst+0xCC):(comment_ofst+0xCC + comment_lngth)]
 					for acctype in types:
 						if account_type == int(acctype):
-							results[username]['Account Type'] = types[acctype]
-					results[username]['RID'] = str(int(x.name().strip("0000"), 16)) #Since im converting hex to int, you need to tell python it's in base 16
-					results[username]['Drive Letter'] = data[(driveletter_ofst+0xCC):(driveletter_ofst+0xCC + driveletter_lngth)]
-					results[username]['Profile Path'] = data[(profilepath_ofst+0xCC):(profilepath_ofst+0xCC + profilepath_lngth)]
-					results[username]['Logon Script'] = data[(logonscript_ofst+0xCC):(logonscript_ofst+0xCC + logonscript_lngth)]
-					results[username]['Workstations'] = data[(workstations_ofst+0xCC):(workstations_ofst+0xCC + workstations_lngth)]
-					# results[username]['LM Password Hash'] = data[(lmpwhash_ofset+0xCC):(lmpwhash_ofset+0xCC + lmpwhash_lngth)]	
-					# results[username]['NT Password Hash'] = data[(ntpwhash_ofset+0xCC):(ntpwhash_ofset+0xCC + ntpwhash_lngth)]	
+							results['users'][username]['Account Type'] = types[acctype]
+					results['users'][username]['RID'] = str(int(x.name().strip("0000"), 16)) #Since im converting hex to int, you need to tell python it's in base 16
+					results['users'][username]['Drive Letter'] = data[(driveletter_ofst+0xCC):(driveletter_ofst+0xCC + driveletter_lngth)]
+					results['users'][username]['Profile Path'] = data[(profilepath_ofst+0xCC):(profilepath_ofst+0xCC + profilepath_lngth)]
+					results['users'][username]['Logon Script'] = data[(logonscript_ofst+0xCC):(logonscript_ofst+0xCC + logonscript_lngth)]
+					results['users'][username]['Workstations'] = data[(workstations_ofst+0xCC):(workstations_ofst+0xCC + workstations_lngth)]
+					# results['users'][username]['LM Password Hash'] = data[(lmpwhash_ofset+0xCC):(lmpwhash_ofset+0xCC + lmpwhash_lngth)]	
+					# results['users'][username]['NT Password Hash'] = data[(ntpwhash_ofset+0xCC):(ntpwhash_ofset+0xCC + ntpwhash_lngth)]	
 
 					continue
 	#Now we combine the two!
 	for RID in f_values:
-		for user in results:
-			if results[user]['RID'] == str(RID):
-				results[user]['Last Login Date'] = f_values[RID][0]
-				results[user]['Password Reset Date'] = f_values[RID][1]
-				results[user]['Password Fail Date'] = f_values[RID][2]
-				results[user]['Account Flags'] = ""			#Gotta do this for the next step
+		for user in results['users']:
+			if results['users'][user]['RID'] == str(RID):
+				results['users'][user]['Last Login Date'] = f_values[RID][0]
+				results['users'][user]['Password Reset Date'] = f_values[RID][1]
+				results['users'][user]['Password Fail Date'] = f_values[RID][2]
+				results['users'][user]['Account Flags'] = ""		
 				for flag in f_values[RID][3]:
-					results[user]['Account Flags'] += (flag + " | ")
-				results[user]['Failed Login Count'] = f_values[RID][4]
-				results[user]['Login Count'] = f_values[RID][5]
+					results['users'][user]['Account Flags'] += (flag + " | ")
+				results['users'][user]['Failed Login Count'] = f_values[RID][4]
+				results['users'][user]['Login Count'] = f_values[RID][5]
 				break
+
+
+	results['groups'] = OrderedDict()
+	#Now to parse the groups!
+	groupsRoot = sam.open("SAM\\Domains\\Builtin\\Aliases")
+	for x in groupsRoot.subkeys():
+		if x.name()[:5] == "00000":			#We dont actually need the Names/Members keys here beacuse everything is in the "C" value
+			#this is going to be the same as the V value, we have to unpack offsets/points to get the data we need
+			#In the C key all offsets are started from 0x34 (this means we do offset+0x34)
+			data = x.value("C").value()
+			name_offst = struct.unpack("<L", data[16:20])[0]
+			name_length = struct.unpack("<L", data[20:24])[0]
+			comment_offst = struct.unpack("<L", data[28:32])[0]
+			comment_lngth = struct.unpack("<L", data[32:36])[0]
+			users_offset = struct.unpack("<L", data[40:44])[0]
+
+			user_count = struct.unpack("<L", data[48:52])[0]
+
+			groupname = data[(name_offst+52):(name_offst+52+name_length)]
+			results['groups'][groupname] = OrderedDict()
+			results['groups'][groupname]['Group Description'] = data[(comment_offst+52):(comment_offst+52+comment_lngth)]
+			results['groups'][groupname]['Last Write'] = x.timestamp()
+			results['groups'][groupname]['User Count'] = user_count
+			results['groups'][groupname]['Memebers'] = ''
+
+
+			try:
+				newOffset = 0
+				for i in range(11, 0,-1):
+					offset = int(users_offset + 52 + newOffset)
+					tmp = struct.unpack("<L", data[offset:offset+4])[0]
+					if tmp == 257:
+						if struct.unpack("<B", data[offset:offset+1])[0] == 0: 
+							offset = offset+1
+						results['groups'][groupname]['Memebers'] += binary_to_sid(data[offset:offset+12]) 
+						#username = sid_to_username(binary_to_sid(data[offset:offset+12]))
+						username = None
+						if username != None:
+							results['groups'][groupname]['Memebers'] += " -> " + username + "</br>"
+						else:
+							results['groups'][groupname]['Memebers'] += "\t\n"
+						newOffset += 12
+					elif tmp == 1281:
+						results['groups'][groupname]['Memebers'] += binary_to_sid(data[offset:offset+28])
+						#username = sid_to_username(binary_to_sid(data[offset:offset+28]))
+						username = None
+						if username != None:
+							results['groups'][groupname]['Memebers'] += " -> " + username + "</br>"
+						else:
+							results['groups'][groupname]['Memebers'] += "\n"
+						newOffset += 28
+
+
+			except:
+				if len(results['groups'][groupname]['Memebers']) == 0:
+					results['groups'][groupname]['Memebers'] = 'No users in this group'
+				else:
+					continue
 
 	return results
 
@@ -156,9 +239,16 @@ if __name__ == "__main__":
 
 	results = samparse(args.SAM)
 
-	for user in results:
+	for user in results['users']:
 		print "----- %s -----" % user
-		for key, value in results[user].iteritems():
+		for key, value in results['users'][user].iteritems():
+			if value != '':
+				print str(key)  + " : " + str(value)
+		print "\n"
+
+	for group in results['groups']:
+		print "----- %s -----" % group
+		for key, value in results['groups'][group].iteritems():
 			if value != '':
 				print str(key)  + " : " + str(value)
 		print "\n"
